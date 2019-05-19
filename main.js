fefa57840dd45535a95ff33972168c2b464537c2
Harry Chung <hc9825@gmail.com>
2019-05-18 22:32:36 -0700

    switched to edamam api, converted json to object

--- main.js
+++ main.js
@@ -6,20 +6,23 @@
 //             "X-RapidAPI-Key": "17f5c1d57emsh9c38ba28668b1a1p103a40jsn5763af6646d2"
 //         },
 //     };
-//     fetch(url, options)
-//         .then(response => {
-//             console.log(response.headers)
-//             console.log(response);
-//             return response.json()})
-//         .then(responseJson => 
-//             console.log(responseJson));
+    // fetch(url, options)
+    //     .then(response => {
+    //         console.log(response.headers)
+    //         console.log(response);
+    //         return response.json()})
+    //     .then(responseJson => 
+    //         console.log(responseJson));
 // }
 // $(test);
 
 "use strict";
 
 const ingredientList = [];
-const spoonacularUrl = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?";
+let recipeList = [];
+const edamamUrl = "https://api.edamam.com/search?";
+const edamamId = "12b959ba";
+const edamamApiKey = "0ff0b20b2cc4c31867eb4788a361d8d2";
 
 function addIngredient() {
     $('form.add-ingredient').submit(event => {
@@ -54,32 +57,61 @@ function paramsFormatted(queryParams) {
     return Object.keys(queryParams).map(key => `${key}=${queryParams[key]}`).join("&");
 }
 
-function callSpoonacularApi() {
+function callEdamamApi() {
+    // https://developer.edamam.com/edamam-docs-recipe-api
     const searchQueryItems = ingredientsFormatted();
     const queryParams = {
-        ranking: 2,
-        ignorePantry: false,
-        ingredients: searchQueryItems,
+        q: searchQueryItems,
+        app_id: edamamId,
+        app_key: edamamApiKey,
+        to: 10
+        // i could add features diet and health later
     }
-    const options = {
-        headers: {
-            "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-            "X-RapidAPI-Key": "17f5c1d57emsh9c38ba28668b1a1p103a40jsn5763af6646d2"
-        },
-    };
-    const spoonacularParams = paramsFormatted(queryParams)
-    console.log(spoonacularParams);
+    
+    const edamamParams = paramsFormatted(queryParams)
+    const url = edamamUrl + edamamParams;
     if (!searchQueryItems) {
         alert("You have no ingredients") 
     } else {
-
+        fetch(url)
+        .then(response => {
+            return response.json()})
+        .then(responseJson => 
+            displayRecipes(responseJson))
+        .catch(err => $('.error-message').html("uh oh, " + err))  
     }
+}
 
+function convertRecipeJson(responseJson) {
+    $('.error-message').empty();
+    recipeList = [];
+    const recipes = responseJson["hits"];
+    recipes.forEach(dish => recipeList.push({
+        name: dish.recipe.label,
+        link: dish.recipe.url,
+        ingredients: dish.recipe.ingredients,
+        calories: dish.recipe.calories,
+        image: dish.recipe.image,
+        macros: {
+            carbs: dish.recipe.totalNutrients.CHOCDF,
+            proteins: dish.recipe.totalNutrients.PROCNT,
+            fats: dish.recipe.totalNutrients.FAT
+        },
+        healthTags: dish.recipe.healthLabels + dish.recipe.dietLabels,
+    }));
+    console.log(recipeList);
+}
+
+function displayRecipes(responseJson) {
+    convertRecipeJson();
+    recipeList.forEach(recipe => {
+        $('recipe-carousel').
+    })
 }
 
 function submitIngredients() {
     $('button.find-recipe').click(event => {
-        callSpoonacularApi();
+        callEdamamApi();
     })
 }
 
